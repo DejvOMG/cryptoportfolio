34bf697132213247a0c5739d4245a0a640886a55
David Srb <david.srb@email.cz>
2025-03-10 23:06:32 +0100

    Inicializace Spring Boot projektu pro správu kryptoměnového portfolia

--- src/main/java/com/example/cryptoportfolio/CryptoportfolioApplication.java
+++ src/main/java/com/example/cryptoportfolio/CryptoportfolioApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CryptoPortfolioApplication {
+public class CryptoportfolioApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CryptoPortfolioApplication.class, args);
+        SpringApplication.run(CryptoportfolioApplication.class, args);
     }
 }
